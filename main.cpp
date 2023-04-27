ff98eb72a451681022e8ed264fbff750949d5c8a
George Davidson <gd240673@gmail.com>
2023-04-26 22:21:17 -0400

    update

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ class Player {
     float playerSpeed;
 
    public:
-    Player(sf::Color color, float playerSpeed, sf::Vector2f playerSize, sf::Vector2f playerPos) 
+    Player(sf::Color playerColor, float playerSpeed, sf::Vector2f playerSize, sf::Vector2f playerPos)
         : playerSpeed(playerSpeed), playerDirection(0.0f, 0.0f) {
-        playerRect.setFillColor(color);
+        playerRect.setFillColor(playerColor);
         playerRect.setSize(playerSize);
         playerRect.setPosition(playerPos);
     }
@@ -53,6 +53,22 @@ class Player {
     }
 };
 
+class Tile {
+   private:
+    sf::RectangleShape tileRect;
+
+   public:
+    Tile(sf::Color tileColor, sf::Vector2f tileSize, sf::Vector2f tilePos) {
+        tileRect.setFillColor(tileColor);
+        tileRect.setSize(tileSize);
+        tileRect.setPosition(tilePos);
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(tileRect);
+    }
+};
+
 int main() {
     std::string windowTitle = "Game";
     int windowWidth = 1200;
@@ -68,28 +84,40 @@ int main() {
     sf::Clock clock;
     float deltaTime;
 
-    Player player(sf::Color::White, 300.0f, sf::Vector2f(48.0f, 48.0f), sf::Vector2f(48.0f, 48.0f));
+    Player player1(sf::Color::White, 300.0f, sf::Vector2f(48.0f, 48.0f), sf::Vector2f(48.0f, 48.0f));
+    Player player2(sf::Color::White, 300.0f, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(192.0f, 192.0f));
+
+    Tile tile1(sf::Color::Black, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(384.0f, 384.0f));
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
                 window.close();
             }
             if (event.type == sf::Event::KeyPressed) {
-                player.verticalMovement(event.key.code, true);
-                player.horizontalMovement(event.key.code, true);
+                player1.verticalMovement(event.key.code, true);
+                player1.horizontalMovement(event.key.code, true);
+
+                player2.verticalMovement(event.key.code, true);
+                player2.horizontalMovement(event.key.code, true);
             }
             if (event.type == sf::Event::KeyReleased) {
-                player.verticalMovement(event.key.code, false);
-                player.horizontalMovement(event.key.code, false);
+                player1.verticalMovement(event.key.code, false);
+                player1.horizontalMovement(event.key.code, false);
+
+                player2.verticalMovement(event.key.code, false);
+                player2.horizontalMovement(event.key.code, false);
             }
         }
         deltaTime = clock.restart().asSeconds();
-        player.update(deltaTime);
+        player1.update(deltaTime);
+        player2.update(deltaTime);
 
         window.clear(sf::Color(64, 64, 64, 255));
 
-        player.draw(window);
+        player1.draw(window);
+        player2.draw(window);
+        tile1.draw(window);
 
         window.display();
     }
