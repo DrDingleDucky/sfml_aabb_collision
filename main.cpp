6551f431829e0e8f0f9c0205162cd47d5839d685
George Davidson <gd240673@gmail.com>
2023-04-27 22:23:09 -0400

    update

--- main.cpp
+++ main.cpp
@@ -2,68 +2,6 @@
 #include <iostream>
 #include <string>
 
-class Player {
-   private:
-    float playerSpeed;
-
-   public:
-    sf::Vector2f playerDirection;
-    sf::RectangleShape playerRect;
-    Player(sf::Color playerColor, float playerSpeed, sf::Vector2f playerSize, sf::Vector2f playerPos)
-        : playerSpeed(playerSpeed), playerDirection(0.0f, 0.0f) {
-        playerRect.setFillColor(playerColor);
-        playerRect.setSize(playerSize);
-        playerRect.setPosition(playerPos);
-    }
-
-    // void horizontalCollisions() {
-
-    // }
-
-    // void verticalCollisions() {
-
-    // }
-
-    void horizontalMovement(sf::Keyboard::Key key, bool isPressed) {
-        if (isPressed) {
-            if (key == sf::Keyboard::A) {
-                playerDirection.x = -1.0f;
-            } else if (key == sf::Keyboard::D) {
-                playerDirection.x = 1.0f;
-            }
-        } else {
-            if ((key == sf::Keyboard::A && playerDirection.x == -1.0f) || (key == sf::Keyboard::D && playerDirection.x == 1.0f)) {
-                playerDirection.x = 0.0f;
-            }
-        }
-    }
-
-    void verticalMovement(sf::Keyboard::Key key, bool isPressed) {
-        if (isPressed) {
-            if (key == sf::Keyboard::W) {
-                playerDirection.y = -1.0f;
-            } else if (key == sf::Keyboard::S) {
-                playerDirection.y = 1.0f;
-            }
-        } else {
-            if ((key == sf::Keyboard::W && playerDirection.y == -1.0f) || (key == sf::Keyboard::S && playerDirection.y == 1.0f)) {
-                playerDirection.y = 0.0f;
-            }
-        }
-    }
-
-    void update(float deltaTime) {
-        // horizontalCollisions();
-        // verticalCollisions();
-
-        playerRect.move(playerDirection * deltaTime * playerSpeed);
-    }
-
-    void draw(sf::RenderWindow& window) {
-        window.draw(playerRect);
-    }
-};
-
 class Tile {
    public:
     sf::RectangleShape tileRect;
@@ -75,6 +13,78 @@ class Tile {
 
     void draw(sf::RenderWindow& window) {
         window.draw(tileRect);
+    }
+};
+
+class Player {
+   private:
+    float playerSpeed;
+    Tile tile;
+
+   public:
+    sf::Vector2f playerDirection;
+    sf::RectangleShape playerRect;
+    Player(sf::Color playerColor, float playerSpeed, sf::Vector2f playerSize, sf::Vector2f playerPos, Tile tile)
+        : playerSpeed(playerSpeed), playerDirection(0.0f, 0.0f), tile(tile) {
+        playerRect.setFillColor(playerColor);
+        playerRect.setSize(playerSize);
+        playerRect.setPosition(playerPos);
+    }
+
+    void horizontalMovement(float deltaTime) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            playerDirection.x = -1;
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            playerDirection.x = 1;
+        } else {
+            playerDirection.x = 0;
+        }
+
+        playerRect.move(sf::Vector2f(playerDirection.x * deltaTime * playerSpeed, 0.0f));
+    }
+
+    void horizontalCollisions() {
+        if (playerRect.getGlobalBounds().intersects(tile.tileRect.getGlobalBounds())) {
+            if (playerDirection.x > 0) {
+                playerRect.setPosition(sf::Vector2f(tile.tileRect.getGlobalBounds().left - playerRect.getSize().x, playerRect.getPosition().y));
+            } else if (playerDirection.x < 0) {
+                playerRect.setPosition(sf::Vector2f(tile.tileRect.getGlobalBounds().left + tile.tileRect.getSize().x, playerRect.getPosition().y));
+            }
+        }
+    }
+
+    void verticalMovement(float deltaTime) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            playerDirection.y = -1;
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            playerDirection.y = 1;
+        } else {
+            playerDirection.y = 0;
+        }
+
+        playerRect.move(sf::Vector2f(0 ,playerDirection.y * deltaTime * playerSpeed));
+    }
+
+    void verticalCollisions() {
+        if (playerRect.getGlobalBounds().intersects(tile.tileRect.getGlobalBounds())) {
+            if (playerDirection.y > 0) {
+                playerRect.setPosition(sf::Vector2f(playerRect.getPosition().x, tile.tileRect.getGlobalBounds().top - playerRect.getSize().y));
+            } else if (playerDirection.y < 0) {
+                playerRect.setPosition(sf::Vector2f(playerRect.getPosition().x, tile.tileRect.getGlobalBounds().top + tile.tileRect.getSize().y));
+            }
+        }
+    }
+
+    void update(float deltaTime) {
+        horizontalMovement(deltaTime);
+        horizontalCollisions();
+
+        verticalMovement(deltaTime);
+        verticalCollisions();
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(playerRect);
     }
 };
 
@@ -93,56 +103,22 @@ int main() {
     sf::Clock clock;
     float deltaTime;
 
-    Player player1(sf::Color::White, 300.0f, sf::Vector2f(48.0f, 48.0f), sf::Vector2f(48.0f, 48.0f));
-    Player player2(sf::Color::White, 300.0f, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(192.0f, 192.0f));
-
-    Tile tile1(sf::Color::Black, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(384.0f, 384.0f));
+    Tile tile(sf::Color::Black, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(384.0f, 384.0f));
+    Player player1(sf::Color::White, 100.0f, sf::Vector2f(48.0f, 48.0f), sf::Vector2f(48.0f, 48.0f), tile);
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
                 window.close();
             }
-            if (event.type == sf::Event::KeyPressed) {
-                player1.verticalMovement(event.key.code, true);
-                player1.horizontalMovement(event.key.code, true);
-
-                player2.verticalMovement(event.key.code, true);
-                player2.horizontalMovement(event.key.code, true);
-            }
-            if (event.type == sf::Event::KeyReleased) {
-                player1.verticalMovement(event.key.code, false);
-                player1.horizontalMovement(event.key.code, false);
-
-                player2.verticalMovement(event.key.code, false);
-                player2.horizontalMovement(event.key.code, false);
-            }
         }
         deltaTime = clock.restart().asSeconds();
         player1.update(deltaTime);
-        player2.update(deltaTime);
-
-        if (player1.playerRect.getGlobalBounds().intersects(tile1.tileRect.getGlobalBounds())) {
-            if (player1.playerDirection.x > 0) {
-                player1.playerRect.setPosition(sf::Vector2f(tile1.tileRect.getGlobalBounds().left - player1.playerRect.getSize().x, player1.playerRect.getPosition().y));
-            } else if (player1.playerDirection.x < 0) {
-                player1.playerRect.setPosition(sf::Vector2f(tile1.tileRect.getGlobalBounds().left + tile1.tileRect.getSize().x, player1.playerRect.getPosition().y));
-            }
-        }
-
-        if (player1.playerRect.getGlobalBounds().intersects(tile1.tileRect.getGlobalBounds())) {
-            if (player1.playerDirection.y > 0) {
-                player1.playerRect.setPosition(sf::Vector2f(player1.playerRect.getPosition().x, tile1.tileRect.getGlobalBounds().top - player1.playerRect.getSize().y));
-            } else if (player1.playerDirection.y < 0) {
-                player1.playerRect.setPosition(sf::Vector2f(player1.playerRect.getPosition().x, tile1.tileRect.getGlobalBounds().top + tile1.tileRect.getSize().y));
-            }
-        }
 
         window.clear(sf::Color(64, 64, 64, 255));
 
         player1.draw(window);
-        player2.draw(window);
-        tile1.draw(window);
+        tile.draw(window);
 
         window.display();
     }
---
+++ test.cpp
@@ -0,0 +1,139 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+class Player {
+   private:
+    float playerSpeed;
+
+   public:
+    sf::Vector2f playerDirection;
+    sf::RectangleShape playerRect;
+    Player(sf::Color playerColor, float playerSpeed, sf::Vector2f playerSize, sf::Vector2f playerPos)
+        : playerSpeed(playerSpeed), playerDirection(0.0f, 0.0f) {
+        playerRect.setFillColor(playerColor);
+        playerRect.setSize(playerSize);
+        playerRect.setPosition(playerPos);
+    }
+
+    void horizontalMovement(sf::Keyboard::Key key, bool isPressed) {
+        if (isPressed) {
+            if (key == sf::Keyboard::A) {
+                playerDirection.x = -1.0f;
+            } else if (key == sf::Keyboard::D) {
+                playerDirection.x = 1.0f;
+            }
+        } else {
+            if ((key == sf::Keyboard::A && playerDirection.x == -1.0f) || (key == sf::Keyboard::D && playerDirection.x == 1.0f)) {
+                playerDirection.x = 0.0f;
+            }
+        }
+    }
+
+    void verticalMovement(sf::Keyboard::Key key, bool isPressed) {
+        if (isPressed) {
+            if (key == sf::Keyboard::W) {
+                playerDirection.y = -1.0f;
+            } else if (key == sf::Keyboard::S) {
+                playerDirection.y = 1.0f;
+            }
+        } else {
+            if ((key == sf::Keyboard::W && playerDirection.y == -1.0f) || (key == sf::Keyboard::S && playerDirection.y == 1.0f)) {
+                playerDirection.y = 0.0f;
+            }
+        }
+    }
+
+    void update(float deltaTime) {
+        playerRect.move(playerDirection * deltaTime * playerSpeed);
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(playerRect);
+    }
+};
+
+class Tile {
+   public:
+    sf::RectangleShape tileRect;
+    Tile(sf::Color tileColor, sf::Vector2f tileSize, sf::Vector2f tilePos) {
+        tileRect.setFillColor(tileColor);
+        tileRect.setSize(tileSize);
+        tileRect.setPosition(tilePos);
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(tileRect);
+    }
+};
+
+int main() {
+    std::string windowTitle = "Game";
+    int windowWidth = 1200;
+    int windowHeight = 816;
+
+    int windowPositionX = sf::VideoMode::getDesktopMode().width / 2 - windowWidth / 2;
+    int windowPositionY = sf::VideoMode::getDesktopMode().height / 2 - windowHeight / 2;
+
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), windowTitle, sf::Style::Close);
+    window.setPosition(sf::Vector2i(windowPositionX, windowPositionY));
+    sf::Event event;
+
+    sf::Clock clock;
+    float deltaTime;
+
+    Player player1(sf::Color::White, 300.0f, sf::Vector2f(48.0f, 48.0f), sf::Vector2f(48.0f, 48.0f));
+    Player player2(sf::Color::White, 300.0f, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(192.0f, 192.0f));
+
+    Tile tile1(sf::Color::Black, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(384.0f, 384.0f));
+
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed) {
+                player1.verticalMovement(event.key.code, true);
+                player1.horizontalMovement(event.key.code, true);
+
+                player2.verticalMovement(event.key.code, true);
+                player2.horizontalMovement(event.key.code, true);
+            }
+            if (event.type == sf::Event::KeyReleased) {
+                player1.verticalMovement(event.key.code, false);
+                player1.horizontalMovement(event.key.code, false);
+
+                player2.verticalMovement(event.key.code, false);
+                player2.horizontalMovement(event.key.code, false);
+            }
+        }
+        deltaTime = clock.restart().asSeconds();
+        player1.update(deltaTime);
+        player2.update(deltaTime);
+
+        if (player1.playerRect.getGlobalBounds().intersects(tile1.tileRect.getGlobalBounds())) {
+            if (player1.playerDirection.x > 0) {
+                player1.playerRect.setPosition(sf::Vector2f(tile1.tileRect.getGlobalBounds().left - player1.playerRect.getSize().x, player1.playerRect.getPosition().y));
+            } else if (player1.playerDirection.x < 0) {
+                player1.playerRect.setPosition(sf::Vector2f(tile1.tileRect.getGlobalBounds().left + tile1.tileRect.getSize().x, player1.playerRect.getPosition().y));
+            }
+        }
+
+        if (player1.playerRect.getGlobalBounds().intersects(tile1.tileRect.getGlobalBounds())) {
+            if (player1.playerDirection.y > 0) {
+                player1.playerRect.setPosition(sf::Vector2f(player1.playerRect.getPosition().x, tile1.tileRect.getGlobalBounds().top - player1.playerRect.getSize().y));
+            } else if (player1.playerDirection.y < 0) {
+                player1.playerRect.setPosition(sf::Vector2f(player1.playerRect.getPosition().x, tile1.tileRect.getGlobalBounds().top + tile1.tileRect.getSize().y));
+            }
+        }
+
+        window.clear(sf::Color(64, 64, 64, 255));
+
+        player1.draw(window);
+        player2.draw(window);
+        tile1.draw(window);
+
+        window.display();
+    }
+    return 0;
+}
