e312baeedd245657da3859d42cacd975fc6bf888
George Davidson <gd240673@gmail.com>
2023-04-29 23:34:50 -0400

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Tile {
    public:
@@ -19,13 +20,14 @@ class Tile {
 class Player {
    private:
     float playerSpeed;
-    Tile tile;
+    std::vector<Tile> tileGroup;
 
    public:
     sf::Vector2f playerDirection;
     sf::RectangleShape playerRect;
-    Player(sf::Color playerColor, float playerSpeed, sf::Vector2f playerSize, sf::Vector2f playerPos, Tile tile)
-        : playerSpeed(playerSpeed), playerDirection(0.0f, 0.0f), tile(tile) {
+    Player(sf::Color playerColor, float playerSpeed, sf::Vector2f playerSize, sf::Vector2f playerPos, std::vector<Tile>& vector)
+        : playerSpeed(playerSpeed), playerDirection(0.0f, 0.0f) {
+        tileGroup = vector;
         playerRect.setFillColor(playerColor);
         playerRect.setSize(playerSize);
         playerRect.setPosition(playerPos);
@@ -44,11 +46,13 @@ class Player {
     }
 
     void horizontalCollisions() {
-        if (playerRect.getGlobalBounds().intersects(tile.tileRect.getGlobalBounds())) {
-            if (playerDirection.x > 0) {
-                playerRect.setPosition(sf::Vector2f(tile.tileRect.getGlobalBounds().left - playerRect.getSize().x, playerRect.getPosition().y));
-            } else if (playerDirection.x < 0) {
-                playerRect.setPosition(sf::Vector2f(tile.tileRect.getGlobalBounds().left + tile.tileRect.getSize().x, playerRect.getPosition().y));
+        for (auto& tile : tileGroup) {
+            if (playerRect.getGlobalBounds().intersects(tile.tileRect.getGlobalBounds())) {
+                if (playerDirection.x > 0) {
+                    playerRect.setPosition(sf::Vector2f(tile.tileRect.getGlobalBounds().left - playerRect.getSize().x, playerRect.getPosition().y));
+                } else if (playerDirection.x < 0) {
+                    playerRect.setPosition(sf::Vector2f(tile.tileRect.getGlobalBounds().left + tile.tileRect.getSize().x, playerRect.getPosition().y));
+                }
             }
         }
     }
@@ -66,11 +70,13 @@ class Player {
     }
 
     void verticalCollisions() {
-        if (playerRect.getGlobalBounds().intersects(tile.tileRect.getGlobalBounds())) {
-            if (playerDirection.y > 0) {
-                playerRect.setPosition(sf::Vector2f(playerRect.getPosition().x, tile.tileRect.getGlobalBounds().top - playerRect.getSize().y));
-            } else if (playerDirection.y < 0) {
-                playerRect.setPosition(sf::Vector2f(playerRect.getPosition().x, tile.tileRect.getGlobalBounds().top + tile.tileRect.getSize().y));
+        for (auto& tile : tileGroup) {
+            if (playerRect.getGlobalBounds().intersects(tile.tileRect.getGlobalBounds())) {
+                if (playerDirection.y > 0) {
+                    playerRect.setPosition(sf::Vector2f(playerRect.getPosition().x, tile.tileRect.getGlobalBounds().top - playerRect.getSize().y));
+                } else if (playerDirection.y < 0) {
+                    playerRect.setPosition(sf::Vector2f(playerRect.getPosition().x, tile.tileRect.getGlobalBounds().top + tile.tileRect.getSize().y));
+                }
             }
         }
     }
@@ -103,9 +109,32 @@ int main() {
     sf::Clock clock;
     float deltaTime;
 
-    Tile tile(sf::Color::Black, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(384.0f, 384.0f));
-    Player player1(sf::Color::Red, 225.0f, sf::Vector2f(96.0f, 96.0f), sf::Vector2f(48.0f, 48.0f), tile);
-    Player player2(sf::Color::Blue, 225.0f, sf::Vector2f(48.0f, 48.0f), sf::Vector2f(288.0f, 288.0f), tile);
+    std::vector<Tile> tileGroup;
+
+    float tileSizeX = 48.0f;
+    float tileSizeY = 48.0f;
+    float tilePosX = 48.0f;
+    float tilePosY = 48.0f;
+    for (int i = 0; i < 3 ;i++) {
+        tileSizeX = tileSizeX + 24.0f;
+        tileSizeY = tileSizeY + 48.0f;
+        tilePosX = tilePosX + 96.0f;
+        tilePosY = tilePosY + 144.0f;
+
+        tileGroup.push_back(Tile(sf::Color::Black, sf::Vector2f(tileSizeX, tileSizeY), sf::Vector2f(tilePosX, tilePosY)));
+
+        tileSizeX = tileSizeX - 24;
+        tileSizeY = tileSizeY + 48;
+        tilePosX = (tilePosX - 96.0f) + 288.0f;
+        tilePosY = (tilePosY - 144.0f) + 96.0f;
+
+        tileGroup.push_back(Tile(sf::Color::Black, sf::Vector2f(tileSizeX, tileSizeY), sf::Vector2f(tilePosX, tilePosY)));
+    }
+
+    tileGroup.push_back(Tile(sf::Color::Black, sf::Vector2f(288, 96), sf::Vector2f(96, 480)));
+    tileGroup.push_back(Tile(sf::Color::Black, sf::Vector2f(288, 96), sf::Vector2f(600, 96)));
+
+    Player player(sf::Color::White, 225.0f, sf::Vector2f(48.0f, 48.0f), sf::Vector2f(48.0f, 48.0f), tileGroup);
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
@@ -114,14 +143,15 @@ int main() {
             }
         }
         deltaTime = clock.restart().asSeconds();
-        player1.update(deltaTime);
-        player2.update(deltaTime);
+        player.update(deltaTime);
 
         window.clear(sf::Color(64, 64, 64, 255));
 
-        player1.draw(window);
-        player2.draw(window);
-        tile.draw(window);
+        player.draw(window);
+
+        for (auto& tile : tileGroup) {
+            tile.draw(window);
+        }
 
         window.display();
     }
